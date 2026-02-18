f252099477055902b7c2ed3a447f24e09e637a2e
kurs.jan <kurs.jan@gmail.com>
2026-02-18 11:01:44 +0000

    Install VIM mode.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -15,7 +15,7 @@
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "vscodevim.vim"
       "google.gemini-cli-vscode-ide-companion"
     ];
     workspace = {
