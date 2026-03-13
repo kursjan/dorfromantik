758bac63d4fb05daa1ec7cce90ce0a55dbcd5b51
Jan Kurs <kursjan@fit.cvut.cz>
2026-03-13 19:57:53 +0100

    feat(di): wire ServiceProvider into App (task 3.2)

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainMenu from './pages/MainMenu';
 import { GameBoard } from './pages/GameBoard';
 import { SessionProvider } from './context/SessionProvider';
+import { ServiceProvider } from './services/ServiceProvider';
 
 function App() {
   return (
-    <SessionProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainMenu />} />
-          <Route path="/game" element={<GameBoard />} />
-        </Routes>
-      </BrowserRouter>
-    </SessionProvider>
+    <ServiceProvider>
+      <SessionProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainMenu />} />
+            <Route path="/game" element={<GameBoard />} />
+          </Routes>
+        </BrowserRouter>
+      </SessionProvider>
+    </ServiceProvider>
   );
 }
 
